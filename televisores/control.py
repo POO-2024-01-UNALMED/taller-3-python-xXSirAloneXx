5b963151bcabc2176d569dc3d3300736e6d462ce
xXSirAloneXx <jsalazargo@unal.edu.co>
2024-02-26 17:46:30 -0500

    00

--- televisores/control.py
+++ televisores/control.py
@@ -9,31 +9,39 @@ class Control():
 
 
     def turnOn(self):
-        self._tv.turnOn()
+        if self._tv:
+            self._tv.turnOn()
     
     def turnOff(self):
-        self._tv.turnOff()
+        if self._tv:
+            self._tv.turnOff()
 
     
     def canalUp(self):
-        self._tv.canalUp()
+        if self._tv:
+            self._tv.canalUp()
 
     def canalDown(self):
-        self._tv.canalDown()
+        if self._tv:
+            self._tv.canalDown()
 
 
     def volumenUp(self):
-        self._tv.volumenUp()
+        if self._tv:
+            self._tv.volumenUp()
     
     def volumenDown(self):
-        self._tv.volumenDown()
+        if self._tv:
+            self._tv.volumenDown()
     
 
     def setCanal(self, canal):
-        self._tv.setCanal(canal)
+        if self._tv:
+            self._tv.setCanal(canal)
     
     def setVolumen(self, volumen):
-        self._tv.setVolumen(volumen)
+        if self._tv:
+            self._tv.setVolumen(volumen)
 
 
 

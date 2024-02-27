ec4c090445ced4407aea23569bc6693bc7dac043
xXSirAloneXx <jsalazargo@unal.edu.co>
2024-02-26 20:46:23 -0500

    666

--- televisores/tv.py
+++ televisores/tv.py
@@ -3,9 +3,9 @@ class TV:
     def __init__(self, marca, estado):
         self._marca = marca
         self._estado = estado
-        self._volumen = 1
-        self._precio = 500
         self._canal = 1
+        self._precio = 500
+        self._volumen = 1
         self._control = None
 
         TV._numTV += 1
